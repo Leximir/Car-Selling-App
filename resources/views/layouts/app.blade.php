10175ceaa8b8504dbf11b9c5120648ff9d3e8c29
Leximir <aleksa.stankovic@proton.me>
2025-11-27 22:37:17 +0100

    Added footer links section for blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,12 @@
 
         @yield('content')
 
-        <footer></footer>
+        <footer>
+            @section('footerLinks')
+                <a href="">Link 1</a>
+                <a href="">Link 2</a>
+            @show
+        </footer>
 
     @endsection
 
