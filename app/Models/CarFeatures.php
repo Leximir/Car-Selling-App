88a79203486dd64f4e9f772cb8773e47a9eb5e9c
Leximir <aleksa.stankovic@proton.me>
2025-12-09 23:12:18 +0100

    Defined $fillable on all models

--- app/Models/CarFeatures.php
+++ app/Models/CarFeatures.php
@@ -10,4 +10,19 @@ class CarFeatures extends Model
     use HasFactory;
 
     public $timestamps = false;
+
+    protected $fillable = [
+        'bluetooth_connectivity',
+        'leather_seats',
+        'gps_navigation',
+        'climate_control',
+        'rear_parking_sensors',
+        'remote_start',
+        'cruise_control',
+        'heated_seats',
+        'power_door_locks',
+        'power_windows',
+        'air_conditioning',
+        'abs'
+    ];
 }
