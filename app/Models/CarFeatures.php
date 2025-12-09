c3331e6ecad7e37bfc757a85354a9ba95e90f5a4
Leximir <aleksa.stankovic@proton.me>
2025-12-09 23:13:37 +0100

    Defined different primary key on CarFeatures.php

--- app/Models/CarFeatures.php
+++ app/Models/CarFeatures.php
@@ -11,6 +11,8 @@ class CarFeatures extends Model
 
     public $timestamps = false;
 
+    protected $primaryKey = 'car_id';
+
     protected $fillable = [
         'bluetooth_connectivity',
         'leather_seats',
