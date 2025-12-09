3df992b0ee8a87cd920256dad1d86bb9eb3ea2fd
Leximir <aleksa.stankovic@proton.me>
2025-12-09 18:18:38 +0100

    Added HasFactory and SoftDeletes to necessary models

--- app/Models/State.php
+++ app/Models/State.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class State extends Model
 {
-    //
+    use HasFactory;
 }
