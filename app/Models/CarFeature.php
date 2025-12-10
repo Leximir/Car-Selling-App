cb765b9339c53e330fb8b8a4c75365f6fdc5c806
Leximir <aleksa.stankovic@proton.me>
2025-12-10 13:20:49 +0100

    One to one relations in Car.php

--- app/Models/CarFeature.php
+++ app/Models/CarFeature.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CarFeatures extends Model
+class CarFeature extends Model
 {
     use HasFactory;
 
