270632cfa8ed9e6773f1247961d262e218c03672
Leximir <aleksa.stankovic@proton.me>
2025-12-10 16:36:06 +0100

    Every factory made

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Maker;
+use App\Models\User;
+
 class HomeController extends Controller
 {
     public function index()
     {
+
+        Maker::factory()
+            ->count(5)
+            ->hasModels(5)
+            ->create();
+
         return view('home.index');
     }
 }
