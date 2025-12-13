20dfad2d366c6417b6d941d50c8d7ca82679b52f
Leximir <aleksa.stankovic@proton.me>
2025-12-13 22:51:54 +0100

    Created the absolute seeder

--- database/factories/CarImageFactory.php
+++ database/factories/CarImageFactory.php
@@ -18,11 +18,7 @@ class CarImageFactory extends Factory
     public function definition(): array
     {
         return [
-            'image_path' => function (array $attributes) {
-                $car = Car::find($attributes['car_id']);
-                return sprintf("https://placehold.co/600x400/gray/white/png?text=%s",
-                $car->make->name);
-            },
+            'image_path' => fake()->imageUrl(),
             'position' => function(array $attributes){
                 return Car::find($attributes['car_id'])->images()->count() + 1;
             }
