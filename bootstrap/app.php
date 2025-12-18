1241222229ca78d5576fa63525751d48bdf68482
Leximir <aleksa.stankovic@proton.me>
2025-12-18 22:18:38 +0100

    Possible disable csfr

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+//        $middleware->validateCsrfTokens(except: [
+//            'stripe/*',
+//            'car'
+//        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
