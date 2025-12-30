daee5b635981c8cf358f4417cffeeb2a4b4320a2
Leximir <aleksa.stankovic@proton.me>
2025-12-31 00:56:46 +0100

    Quick optimization

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -7,7 +7,6 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Password;
 
 class SignupController extends Controller
 {
